5beea7f94046cfcb75cf2190691bed425c08eb19
Aliaksandr Artsisheuski <artsisheuski24@gmail.com>
2022-02-20 15:39:06 +0300

    added class Shape

---
+++ Shapes/Shape.py
@@ -0,0 +1,14 @@
+from typing import Any
+
+
+class Shape:
+    def __init__(self, center = 8, out_color = 'black'):
+        self.__center = center
+        self.__out_color = out_color
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        super().__setattr__(name, value)
+
+    def __getattribute__(self, name: str) -> Any:
+        return super().__getattribute__(name)
+
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from tkinter import *
+from Shapes import Shape
+
 
 
 window = Tk()
 window.title("Добро пожаловать в приложение PythonRu")
-window.mainloop()
+canvas = Canvas()
+canvas.create_polygon([50,40,120,80], outline = 'black', width = 5)
+canvas.pack(fill=BOTH)
+#window.mainloop()
+
+shape = Shape()
+##print(shape.__getattribute__('center'))
