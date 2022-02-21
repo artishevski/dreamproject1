4700e38644ad9fae42ffa6e3aa823802976a87ba
Aliaksandr Artsisheuski <artsisheuski24@gmail.com>
2022-02-21 18:51:58 +0300

    added figures to menu

--- GUI/MainScreen.py
+++ GUI/MainScreen.py
@@ -13,10 +13,13 @@ class MainScreen:
         figure_choser.add_command(label = 'interval')
         figure_choser.add_command(label = 'triangle')
         figure_choser.add_command(label = 'rectangle')
+        figure_choser.add_command(label = 'diamond')
         figure_choser.add_command(label = 'equilateral triangle')
         figure_choser.add_command(label = 'square')
         figure_choser.add_command(label = 'equilateral pentagon')
         figure_choser.add_command(label = 'equilateral hexagon')
+        figure_choser.add_command(label = 'ellipsis')
+        figure_choser.add_command(label = 'circle')
         menu.add_cascade(label = 'Choose a figure', menu = figure_choser)
 
         out_color_choser = Menu(menu, tearoff = 0)
