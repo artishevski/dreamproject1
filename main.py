1a60ab6d7215d068efeffb4c475b218c13ebaa4f
Aliaksandr Artsisheuski <artsisheuski24@gmail.com>
2022-02-21 18:48:26 +0300

    changes in main.py

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 from tkinter import *
 from Shapes.Shape import *
 
-
-
-#window = Tk()
-#window.title("Добро пожаловать в приложение PythonRu")
-#canvas = Canvas()
-#canvas.create_polygon([50,40,120,80], outline = 'black', width = 5)
-#canvas.pack(fill=BOTH)
-#window.mainloop()
-
 shape = Shape()
 print(shape._center)
