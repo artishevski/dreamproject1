4c784023e8fe573a138396211d4eb918c4dfacf1
Theodore Piatrovich <theodore.ptr.official@gmail.com>
2022-03-19 21:19:42 +0300

    Move figure feature added

--- shapes/Shape.py
+++ shapes/Shape.py
@@ -6,6 +6,6 @@ class Shape:
         self._center = center
         self.__out_color = out_color
 
-    def move_center(self, new_coords: Point):
+    def move_center(self, new_coords):
         self._center = new_coords
 
